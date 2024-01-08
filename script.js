d9497c4b4ea8139a58f18cd362bbf06f04a9ef79
blazion09 <muhd09afiq@gmail.com>
2024-01-08 12:19:49 +0800

    complete starting point

--- script.js
+++ script.js
@@ -11,8 +11,6 @@ const divide = function (a, b) {
   return a / b;
 };
 
-let firstInteger = 0;
-let secondInteger = 0;
 let operator = "";
 let total = 0;
 
@@ -23,58 +21,68 @@ const operate = function (operator, a, b) {
 //Create the functions that populate the display when you click the number buttons.
 //You should be storing the ‘display value’ in a variable somewhere for use in the next step.
 let currentValue = 0;
-let displayValue = function () {
-  document.querySelector(".display").textContent = currentValue;
-};
+let integer1 = 0;
 
+let displayValue = function (){
+    document.querySelector(".display").textContent = currentValue;
+};
 const btn1 = document.getElementById("btn1").addEventListener("click", () => {
   currentValue = currentValue * 10 + 1;
-  firstInteger = currentValue;
+  integer1 = integer1 * 10 + 1;
   displayValue();
 });
+
 const btn2 = document.getElementById("btn2").addEventListener("click", () => {
   currentValue = currentValue * 10 + 2;
-  firstInteger = currentValue;
+  integer1 = integer1 * 10 + 2;
   displayValue();
 });
+
 const btn3 = document.getElementById("btn3").addEventListener("click", () => {
   currentValue = currentValue * 10 + 3;
-  firstInteger = currentValue;
+  integer1 = integer1 * 10 + 3;
   displayValue();
 });
+
 const btn4 = document.getElementById("btn4").addEventListener("click", () => {
   currentValue = currentValue * 10 + 4;
-  firstInteger = currentValue;
+  integer1 = integer1 * 10 + 4;
   displayValue();
 });
+
 const btn5 = document.getElementById("btn5").addEventListener("click", () => {
   currentValue = currentValue * 10 + 5;
-  firstInteger = currentValue;
+  integer1 = integer1 * 10 + 5;
   displayValue();
 });
+
 const btn6 = document.getElementById("btn6").addEventListener("click", () => {
   currentValue = currentValue * 10 + 6;
-  firstInteger = currentValue;
+  integer1 = integer1 * 10 + 6;
   displayValue();
 });
+
 const btn7 = document.getElementById("btn7").addEventListener("click", () => {
   currentValue = currentValue * 10 + 7;
-  firstInteger = currentValue;
+  integer1 = integer1 * 10 + 7;
   displayValue();
 });
+
 const btn8 = document.getElementById("btn8").addEventListener("click", () => {
   currentValue = currentValue * 10 + 8;
-  firstInteger = currentValue;
+  integer1 = integer1 * 10 + 8;
   displayValue();
 });
+
 const btn9 = document.getElementById("btn9").addEventListener("click", () => {
   currentValue = currentValue * 10 + 9;
-  firstInteger = currentValue;
+  integer1 = integer1 * 10 + 9;
   displayValue();
 });
+
 const btn0 = document.getElementById("btn0").addEventListener("click", () => {
   currentValue = currentValue * 10 + 0;
-  firstInteger = currentValue;
+  integer1 = integer1 * 10 + 0;
   displayValue();
 });
 
@@ -82,7 +90,6 @@ const clearValue = document
   .getElementById("clear")
   .addEventListener("click", () => {
     currentValue = 0;
-    total = 0;
     displayValue();
   });
 
@@ -90,54 +97,10 @@ const clearValue = document
 //into the calculator, utilize the operator that the user selects,
 //and then operate() on the two numbers when the user presses the “=” key.
 
-const addBtn = document.getElementById("add").addEventListener("click", () => {
-  operator = "+";
-  calculation();
-});
+const addBtn = document
+  .getElementById("add")
+  .addEventListener("click", () => {});
 
 const subtractBtn = document
   .getElementById("subtract")
-  .addEventListener("click", () => {
-    operator = "-";
-    calculation();
-  });
-
-const multiplyBtn = document
-  .getElementById("multiply")
-  .addEventListener("click", () => {
-    operator = "x";
-    calculation();
-  });
-
-const divideBtn = document
-  .getElementById("divide")
-  .addEventListener("click", () => {
-    operator = "/";
-    calculation();
-  });
-
-let calculation = function () {
-  switch (operator) {
-    case "+":
-      total = add(firstInteger, total);
-      break;
-    case "-":
-      total = subtract(firstInteger, total);
-      break;
-    case "x":
-      total = multiply(firstInteger, total);
-      break;
-    case "/":
-      total = divide(firstInteger, total);
-      break;
-  }
-  currentValue = total;
-  displayValue();
-  currentValue = 0;
-  firstInteger = total;
-};
-
-const equal = document.getElementById("equal").addEventListener("click", () => {
-  calculation();
-  firstInteger = 0;
-});
+  .addEventListener("click", () => {});
